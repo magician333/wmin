28b947e0340c7d2c880a8a7b1155562603927522
magician333 <magician33333@gmail.com>
2016-04-12 00:02:06 +0800

    Add exception type

--- wmin.py
+++ wmin.py
@@ -36,39 +36,48 @@ else:
     argv.pop(0)  # delete the argv[0]
     for i in range(0, len(argv)):
         if "-u" == argv[i]:  # get url
+
             try:
                 url = argv[i + 1]
                 U = True
-            except:
+            except IndexError:
                 printf("-u No argv!","error")
+
         elif "-d" == argv[i]:  # get dictionary
             try:
                 dictionary = argv[i + 1]
+
                 try:
                     open(dictionary)
                     D = True
-                except:
+                except FileNotFoundError:
                     printf(dictionary+" is not found","error")
                     D = False
                     break
                     
-            except:
+            except IndexError:
                 printf("-d No argv!","error")
+
         elif "-f" == argv[i]:  # get output filename
             try:
                 result_file = argv[i + 1]
-            except:
+            except IndexError:
                 printf("-f No argv!","error")
         elif "-t" == argv[i]:  # set timeout
             try:
+
                 try:
                     timeout = float(argv[i + 1])
                 except ValueError:
                     printf("Must use postitive float,will use default","warning")
-            except:
+
+            except IndexError:
                 printf("-t No argv!","error")
+
         elif "-p" == argv[i]:  # set proxy
+
             try:
+
                 try:
                     if ":" and "~" in argv[i+1]:
                         proxy = dict({argv[i+1].split("~")[1]: argv[i+1].split(":")[0]+":"+argv[i+1].split("~")[0].split(":")[1]})
@@ -77,41 +86,55 @@ else:
                         """
                     else:
                         raise IndexError
+
                 except IndexError:
                     printf("Type Wrong !!!,will use default","warning")
                     proxy = None
-            except:
-                printf(e,"error")
+
+            except IndexError:
                 printf("-p No argv!","error")
+
         elif "-m" == argv[i]:  # set maximum status_code to show
+
             try:
+
                 try:
                     max_code = argv[i + 1]
                 except ValueError:
                     printf("Must use postitive integer,will use default","warning")
-            except:
+
+            except IndexError:
                  printf("-m No argv!","error")
+
         elif "-a" == argv[i]:	#set User-Agent
+
             try:
                 ua = {"User-Agent":argv[i+1]}
-            except:
+            except IndexError:
                 printf("-a No agrv!","error")
+
         elif "-i" == argv[i]:   #set ignore string
+
             try:
                 check_text = argv[i+1]
-            except:
+            except IndexError:
                 printf("-i No argv!","error")
+
         elif "-h" == argv[i]:
             printf(help_doc)
+
         elif 0 == len(argv):
             printf("Please enter the argv!","warning")
+
         else:
+
             if argv[i - 1] in ["-u", "-d", "-f", "-t", "-p","-a","-i","-m"]:
                 pass
             else:
                 printf("Can't find argv: " + argv[i],"warning")
 
     for para_test in argv:
+
         if argv.count(para_test)>1:
             printf("Have two same argv "+para_test,"error")
             U = False
@@ -119,6 +142,7 @@ else:
 
     if 0 == len(result_file):
         result_file == ""
+
     if U ==True and D == True:
         scan.dic_scan(url, dictionary, result_file, timeout, proxy,ua,check_text)
     elif U == True and D == False:
