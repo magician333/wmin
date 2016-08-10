637522be079a945b23d7c6758f49e40018c1a136
magician333 <magician33333@gmail.com>
2016-08-10 14:29:37 +0800

    fix bug ,add pep8 format

--- libs/Url.py
+++ libs/Url.py
@@ -1,36 +1,35 @@
-#coding : utf-8
+# coding : utf-8
 
 import queue
 import time
 import requests
-import threading
 import random
 from . import result
 from sys import version_info
 import socket
 
 if version_info.major == 3:
-    from .printf.py3 import printf,printweb
+    from .printf.py3 import printf, printweb
     from urllib.parse import urlparse
 else:
-    from .printf.py2 import printf,printweb
+    from .printf.py2 import printf, printweb
     from urlparse import urlparse
 
 
 class Url:
     """docstring for Url"""
-    def __init__(self, url,dictionary,timeout,proxy,delay,ua,ignore_text,method):
+    def __init__(self, url, dictionary, timeout,
+                 proxy, delay, ua, ignore_text, method):
         self.url_deal(url)
         self.dictionary = self.deal_dictionary(dictionary)
         self.timeout = timeout
-        self.proxy = list(map(self.build_proxy,proxy))
-        self.ua = list(map(self.build_ua,ua))
+        self.proxy = list(map(self.build_proxy, proxy))
+        self.ua = list(map(self.build_ua, ua))
         self.delay = delay
         self.ignore_text = ignore_text
         self.method = self.filter_method(method)
-        #ipdb.set_trace()
 
-    def deal_dictionary(self,dictionary):
+    def deal_dictionary(self, dictionary):
         self.dict_line = queue.Queue()
 
         def add_dictline(filename):
@@ -46,17 +45,15 @@ class Url:
             except FileNotFoundError:
                 self.dict_line = None
 
-
-        if isinstance(dictionary,list):
-            #dictionarys folder list
+        if isinstance(dictionary, list):
+            # dictionarys folder list
             for filename in dictionary:
                 add_dictline(filename)
         else:
-            #single dictionary file
+            # single dictionary file
             add_dictline(dictionary)
 
-
-    def url_deal(self,url):
+    def url_deal(self, url):
         protocol = urlparse(url).scheme
         if protocol:
             purl = url
@@ -69,25 +66,27 @@ class Url:
             self.url = purl
             self.hostname = hostname
         else:
-            printf("Please enter url with protocl","warning")
+            printf("Please enter url with protocl", "warning")
             exit()
 
-    def filter_method(self,method):
+    def filter_method(self, method):
         if method.lower() not in ["get", "post", "head"]:
             return None
         else:
             return method.lower()
 
-    def build_ua(self,ua):
+    def build_ua(self, ua):
         if "" == ua:
             return None
         else:
             return dict({"User-Agent": ua})
-    def build_proxy(self,proxy):
+
+    def build_proxy(self, proxy):
         if "" != proxy:
             if ":" and "@" in proxy:
                 # proxy type : ip:port@type
-                proxy = dict({proxy.split("@")[1]: proxy.split(":")[0] + ":" + proxy.split("@")[0].split(":")[1]})
+                proxy = dict({proxy.split("@")[1]: proxy.split(":")[0]
+                              + ":" + proxy.split("@")[0].split(":")[1]})
                 return proxy
             else:
                 printf("Type wrong!", "warning")
@@ -107,16 +106,18 @@ class Url:
         try:
             # use appoint method
             if "get" == self.method:
-                code = requests.get(url, timeout=self.timeout, proxies=self.proxy[random.randint(0,len(self.proxy)-1)], headers=self.ua[random.randint(0,len(self.ua)-1)], allow_redirects=False).status_code
+                code = requests.get(url, timeout=self.timeout, proxies=self.proxy[random.randint(0, len(self.proxy)-1)], headers=self.ua[random.randint(0,len(self.ua)-1)], allow_redirects=False).status_code
             elif "post" == self.method:
-                code = requests.post(url, timeout=self.timeout, proxies=self.proxy[random.randint(0,len(self.proxy)-1)], headers=self.ua[random.randint(0,len(self.ua)-1)], allow_redirects=False).status_code
+                code = requests.post(url, timeout=self.timeout, proxies=self.proxy[random.randint(0, len(self.proxy)-1)], headers=self.ua[random.randint(0, len(self.ua)-1)], allow_redirects=False).status_code
             else:
-                code = requests.head(url, timeout=self.timeout, proxies=self.proxy[random.randint(0,len(self.proxy)-1)], headers=self.ua[random.randint(0,len(self.ua)-1)], allow_redirects=False).status_code
+                code = requests.head(url, timeout=self.timeout, proxies=self.proxy[random.randint(0, len(self.proxy)-1)], headers=self.ua[random.randint(0, len(self.ua)-1)], allow_redirects=False).status_code
 
-            if "" != self.ignore_text and self.ignore_text not in requests.get(url).text:
+            if "" != self.ignore_text and \
+               self.ignore_text not in requests.get(url).text:
                 printweb(code, url)
                 if code < 400:
-                    result.export_html(self.report_filename, url, url + "\t" + str(code))
+                    result.export_html(self.report_filename,
+                                       url, url + "\t" + str(code))
             self.dict_line.task_done()
         except KeyboardInterrupt:
             exit()
@@ -132,7 +133,8 @@ class Url:
             except:
                 printf("Can\'t get server,Connect wrong", "error")
             try:
-                printf("IP:\t" +socket.gethostbyname(self.hostname), "normal")
+                printf("IP:\t" +
+                       socket.gethostbyname(self.hostname), "normal")
             except Exception as e:
                 print(e)
                 printf("Can\'t get ip,Connect wrong", "error")
--- wmin.py
+++ wmin.py
@@ -1,6 +1,4 @@
 import argparse
-import os
-from libs import result
 from libs import addon
 from sys import version_info
 from libs import Url
@@ -29,49 +27,63 @@ def main():
     arg.add_argument("-d", type=str, help="set dictionary", metavar="")
     arg.add_argument("-D", type=str, help="set dictionary folder", metavar="")
     # arg.add_argument("-r", type=str, help="set report filename", metavar="")
-    arg.add_argument("-t", type=float, help="set timeout",metavar="", default=0.04)
-    arg.add_argument("-p", type=str, help="set proxy    *format:  ip:port@type",default="", metavar="")
-    arg.add_argument("-P", type=str, help="set proxy file,random read", default="", metavar="")
-    arg.add_argument("-m", type=str, help="set method",default="get", metavar="")
-    arg.add_argument("-e", type=int, help="set delay seconds",default=0, metavar="")
-    arg.add_argument("-a", type=str, help="set User-Agent", metavar="",default="")
-    arg.add_argument("-A", type=str, help="set User-Agent file,random read", default="", metavar="")
-    arg.add_argument("-x", type=int, help="set multithreading number", default=1, metavar="")
-    arg.add_argument("-X", type=int, help="set multiprocessing number", default=1, metavar="")
-    arg.add_argument("-i", type=str, help="set ignore text default 404", default='404', metavar="")
+    arg.add_argument("-t", type=float, help="set timeout",
+                     metavar="", default=0.04)
+    arg.add_argument("-p", type=str,
+                     help="set proxy    *format:  ip:port@type",
+                     default="", metavar="")
+    arg.add_argument("-P", type=str,
+                     help="set proxy file,random read", default="", metavar="")
+    arg.add_argument("-m", type=str,
+                     help="set method", default="get", metavar="")
+    arg.add_argument("-e", type=int,
+                     help="set delay seconds", default=0, metavar="")
+    arg.add_argument("-a", type=str,
+                     help="set User-Agent", metavar="", default="")
+    arg.add_argument("-A", type=str,
+                     help="set User-Agent file,random read",
+                     default="", metavar="")
+    arg.add_argument("-x", type=int,
+                     help="set multithreading number", default=1, metavar="")
+    arg.add_argument("-X", type=int,
+                     help="set multiprocessing number", default=1, metavar="")
+    arg.add_argument("-i", type=str,
+                     help="set ignore text", default='404', metavar="")
 
     args = arg.parse_args()
     para = vars(args)
 
-    url = addon.deal_url(para["u"],para["U"])
-    dictionary = addon.deal_dict(para["d"],para["D"])
+    url = addon.deal_url(para["u"], para["U"])
+    dictionary = addon.deal_dict(para["d"], para["D"])
     # report = addon.deal_report(para["r"])
     timeout = addon.deal_num(para["t"])
-    proxy = addon.batch_deal(para["p"],para["P"])
+    proxy = addon.batch_deal(para["p"], para["P"])
     delay = addon.deal_num(para["e"])
-    ua = addon.batch_deal(para["a"],para["A"])
+    ua = addon.batch_deal(para["a"], para["A"])
     ignore_text = para["i"]
     method = addon.filter_method(para["m"])
 
+    # if url.qsize() > 1 and report:
+    #    printf("If you set URLS,you can not set the output filename", "error")
+    # else:
 
-    if url.qsize() > 1 and report:
-        printf("If you set URLS,you can not set the output filename", "error")
-    else:
-        if url and dictionary == None:
-            for i in range(url.qsize()):
-                target = Url.Url(url.get(), "", timeout, proxy, delay, ua, ignore_text, method)
-                target.get_info()
-        elif url and dictionary:
-            for i in range(url.qsize()):
-                target = Url.Url(url.get(), dictionary, timeout, proxy, delay, ua, ignore_text, method)
-                target.build_report_file()
-                target.run()
+    if url and dictionary is None:
+        for i in range(url.qsize()):
+            target = Url.Url(url.get(), "", timeout,
+                             proxy, delay, ua, ignore_text, method)
+            target.get_info()
+    elif url and dictionary:
+        for i in range(url.qsize()):
+            target = Url.Url(url.get(), dictionary,
+                             timeout, proxy, delay, ua, ignore_text, method)
+            target.build_report_file()
+            target.run()
 
 
 if __name__ == '__main__':
-    # try:
-    main()
-    # except:
-    #     printf("Maybe something wrong.........\n","warning")
-    #     exit()
-        
+
+    try:
+        main()
+    except:
+        printf("Maybe something wrong.........\n", "warning")
+        exit()
